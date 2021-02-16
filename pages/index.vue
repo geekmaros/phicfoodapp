3e892c0e3539f6f2fdcdd8da1e3d911e5893d1d9
mustaphic <abdulrasaq.mustapha@demzanalytics.com>
2021-02-16 15:33:14 +0100

    added files for deployment

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <main class="container">
+   <AppHeader></AppHeader>
+   <AppRestaurantInfo></AppRestaurantInfo>
+  </main>
+</template>
+
+<script>
+import AppHeader from '@/components/AppHeader.vue';
+import AppRestaurantInfo from '~/components/AppRestaurantInfo.vue';
+
+export default {
+  components: {
+    AppHeader,
+    AppRestaurantInfo
+  },
+}
+</script>
+
+<style>
+
+</style>
